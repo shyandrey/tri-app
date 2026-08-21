e44747d40120c7113c57d1a5556741f5e61d2b6a
shyandrey <shy.andrey@gmail.com>
2026-08-21 18:37:11 +0300

    Connect Gold Coast and Singapore T100 results

--- src/data/results/index.ts
+++ src/data/results/index.ts
@@ -1,10 +1,14 @@
 import { texas2026Results } from './2026/ironman/texas'
+import { goldCoast2026Results } from './2026/t100/gold-coast'
+import { singapore2026Results } from './2026/t100/singapore'
 import { spain2026Results } from './2026/t100/spain'
 import { sanFrancisco2026Results } from './2026/t100/san-francisco'
 import { vancouver2026Results } from './2026/t100/vancouver'
 
 export const raceResults = [
   ...texas2026Results,
+  ...goldCoast2026Results,
+  ...singapore2026Results,
   ...spain2026Results,
   ...sanFrancisco2026Results,
   ...vancouver2026Results,
