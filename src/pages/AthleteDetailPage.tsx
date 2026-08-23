0e6c6cf889f089aa3c2867a65c7cdd03a2be5723
shyandrey <shy.andrey@gmail.com>
2026-08-23 13:30:51 +0300

    Show English name in athlete profile

--- src/pages/AthleteDetailPage.tsx
+++ src/pages/AthleteDetailPage.tsx
@@ -21,50 +21,52 @@ function AthleteDetailPage({
   onNavigate,
   onRaceClick,
 }: AthleteDetailPageProps) {
-  
   const recentResults = results
-  .map((result) => {
-    const race = races.find(
-      (race) => race.id === result.raceId
-    )
+    .map((result) => {
+      const race = races.find((race) => race.id === result.raceId)
 
-    return {
-      ...result,
-      race,
-    }
-  })
-  .filter((result) => result.race)
-  .sort((a, b) => {
-    return (
-      new Date(b.race!.dateISO).getTime() -
-      new Date(a.race!.dateISO).getTime()
-    )
-  })
-  .slice(0, 3)
-  
-    return (
+      return {
+        ...result,
+        race,
+      }
+    })
+    .filter((result) => result.race)
+    .sort((a, b) => {
+      return (
+        new Date(b.race!.dateISO).getTime() -
+        new Date(a.race!.dateISO).getTime()
+      )
+    })
+    .slice(0, 3)
+
+  return (
     <main className="app">
       <section className="section athlete-detail-page">
-        <button
-          className="race-detail-back"
-          onClick={onBack}
-        >
+        <button className="race-detail-back" onClick={onBack}>
           ← Назад
         </button>
 
         <div className="athlete-detail">
-            {athlete.image && (
-  <img
-    className="athlete-detail__image"
-    src={athlete.image}
-    alt={athlete.name}
-  />
-)}
+          {athlete.image && (
+            <img
+              className="athlete-detail__image"
+              src={athlete.image}
+              alt={athlete.name}
+            />
+          )}
+
           <h1>
-             {athlete.name} <span className="athlete-detail__flag-inline">{athlete.flag}</span>
+            {athlete.name}{' '}
+            <span className="athlete-detail__flag-inline">{athlete.flag}</span>
           </h1>
 
           <p className="athlete-detail__meta">
+            {athlete.nameEn && (
+              <>
+                {athlete.nameEn}
+                <br />
+              </>
+            )}
             {athlete.country} · {athlete.discipline}
           </p>
 
@@ -76,53 +78,49 @@ function AthleteDetailPage({
           <div className="athlete-detail__achievements">
             <h2>Основные достижения</h2>
 
-{recentResults.length > 0 && (
-  <div className="athlete-detail__results">
-    <h2>Недавние результаты</h2>
+            {recentResults.length > 0 && (
+              <div className="athlete-detail__results">
+                <h2>Недавние результаты</h2>
 
-    <div className="athlete-results-list">
-      {recentResults.map((result) => (
-        <article
-          className="athlete-result-card"
-          key={result.id}
-          onClick={() => onRaceClick(result.race!)}
-        >
-          <div className="athlete-result-card__place">
-            {result.position}
-          </div>
+                <div className="athlete-results-list">
+                  {recentResults.map((result) => (
+                    <article
+                      className="athlete-result-card"
+                      key={result.id}
+                      onClick={() => onRaceClick(result.race!)}
+                    >
+                      <div className="athlete-result-card__place">
+                        {result.position}
+                      </div>
 
-          <div className="athlete-result-card__info">
-            <strong>{result.race!.name}</strong>
+                      <div className="athlete-result-card__info">
+                        <strong>{result.race!.name}</strong>
 
-            <span>
-              {result.race!.date} · {result.race!.city}
-            </span>
-          </div>
+                        <span>
+                          {result.race!.date} · {result.race!.city}
+                        </span>
+                      </div>
 
-          <strong className="athlete-result-card__time">
-            {result.totalTime ?? '—'}
-          </strong>
-        </article>
-      ))}
-    </div>
-  </div>
-)}
+                      <strong className="athlete-result-card__time">
+                        {result.totalTime ?? '—'}
+                      </strong>
+                    </article>
+                  ))}
+                </div>
+              </div>
+            )}
 
             <ul>
               {athlete.achievements.map((achievement) => (
-                <li key={achievement}>
-                  {achievement}
-                </li>
+                <li key={achievement}>{achievement}</li>
               ))}
             </ul>
           </div>
         </div>
       </section>
-    <BottomNav
-  currentPage="athletes"
-  onNavigate={onNavigate}
-/>
-</main>
+
+      <BottomNav currentPage="athletes" onNavigate={onNavigate} />
+    </main>
   )
 }
 
