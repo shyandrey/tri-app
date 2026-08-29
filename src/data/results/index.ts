9bac2ef623a57eb3951fcd2dd108511378d13715
shyandrey <shy.andrey@gmail.com>
2026-08-29 20:00:57 +0300

    Include Geelong 2026 results

--- src/data/results/index.ts
+++ src/data/results/index.ts
@@ -1,4 +1,5 @@
 import { newZealand2026Results } from './2026/ironman/new-zealand'
+import { geelong2026Results } from './2026/ironman/geelong'
 import { texas2026Results } from './2026/ironman/texas'
 import { goldCoast2026Results } from './2026/t100/gold-coast'
 import { singapore2026Results } from './2026/t100/singapore'
@@ -8,6 +9,7 @@ import { vancouver2026Results } from './2026/t100/vancouver'
 
 export const raceResults = [
   ...newZealand2026Results,
+  ...geelong2026Results,
   ...texas2026Results,
   ...goldCoast2026Results,
   ...singapore2026Results,
