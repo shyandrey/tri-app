6285b5b04da1efec804c430af149e439dfb104cb
shyandrey <shy.andrey@gmail.com>
2026-09-03 00:19:56 +0300

    Include archive races in race entity catalog

--- src/data/raceEntities.ts
+++ src/data/raceEntities.ts
@@ -1,17 +1,17 @@
 import type { RaceEntity } from '../types/Race'
 import { currentRaceEditions } from './raceEditions'
+import { archiveIronmanRaceEntities } from './archiveIronmanProSeries'
+
+const currentRaceEntities: RaceEntity[] = currentRaceEditions.map((edition) => ({
+  id: edition.raceId,
+  name: edition.name,
+  country: edition.country,
+  city: edition.city,
+  distance: edition.distance,
+}))
 
 export const raceEntities: RaceEntity[] = Array.from(
   new Map(
-    currentRaceEditions.map((edition) => [
-      edition.raceId,
-      {
-        id: edition.raceId,
-        name: edition.name,
-        country: edition.country,
-        city: edition.city,
-        distance: edition.distance,
-      },
-    ])
+    [...archiveIronmanRaceEntities, ...currentRaceEntities].map((race) => [race.id, race])
   ).values()
 )
