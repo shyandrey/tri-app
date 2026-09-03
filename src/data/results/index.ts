e0d951be8238f2a8454a3e730d6edefc6ebb2af1
shyandrey <shy.andrey@gmail.com>
2026-09-03 18:32:54 +0300

    Complete 2025 Ironman Pro Series results

--- src/data/results/index.ts
+++ src/data/results/index.ts
@@ -16,6 +16,8 @@ import { swansea2025Results } from './2025/ironman/swansea'
 import { lakePlacid2025Results } from './2025/ironman/lake-placid'
 import { zellAmSee2025Results } from './2025/ironman/zell-am-see'
 import { niceWorldChampionship2025Results } from './2025/ironman/nice-world-championship'
+import { kona2025Results } from './2025/ironman/kona'
+import { marbellaWomen2025Results, marbellaMen2025Results } from './2025/ironman/marbella-world-championship'
 import { newZealand2026Results } from './2026/ironman/new-zealand'
 import { geelong2026Results } from './2026/ironman/geelong'
 import { oceanside2026Results } from './2026/ironman/oceanside'
@@ -52,6 +54,7 @@ const results2025: RaceResult[] = [
   ...veniceJesolo2025Results, ...stGeorge2025Results, ...aixEnProvence2025Results, ...hamburg2025Results,
   ...eagleman2025Results, ...cairns2025Results, ...frankfurt2025Results, ...swansea2025Results,
   ...lakePlacid2025Results, ...zellAmSee2025Results, ...niceWorldChampionship2025Results,
+  ...kona2025Results, ...marbellaWomen2025Results, ...marbellaMen2025Results,
 ]
 
 const countryCodeByAthlete = new Map<string, string>(Object.entries(athleteCountryCodes))
