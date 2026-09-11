ce7cd2f7cac27e55b64ee75ba8eedf08d89a3fa7
shyandrey <shy.andrey@gmail.com>
2026-09-11 13:36:05 +0300

    Wire remaining 2026 T100 showcase images

--- src/data/showcaseImages.ts
+++ src/data/showcaseImages.ts
@@ -4,10 +4,18 @@ import konaSwimStart from '../assets/showcase/kona/kona-swim-start.png'
 import niceBikeMountains from '../assets/showcase/nice/nice-bike-mountains.png'
 import niceRunPromenade from '../assets/showcase/nice/nice-run-promenade.png'
 import niceSwimStart from '../assets/showcase/nice/nice-swim-start.png'
+import frenchRivieraRun from '../assets/showcase/french-riviera/french-riviera-run.png'
+import dubaiSwimSkyline from '../assets/showcase/dubai/dubai-swim-skyline.png'
+import saudiArabiaSeaview from '../assets/showcase/saudi-arabia/saudi-arabia-seaview.png'
+import qatarLusailSunset from '../assets/showcase/qatar/qatar-lusail-sunset.png'
 
 const showcaseImagesByRaceId: Record<string, string[]> = {
   'ironman-world-championship-kona': [konaBikeLava, konaSwimStart, konaFinishAlii],
   'ironman-70-3-world-championship': [niceBikeMountains, niceSwimStart, niceRunPromenade],
+  't100-french-riviera': [frenchRivieraRun],
+  't100-dubai': [dubaiSwimSkyline],
+  't100-saudi-arabia': [saudiArabiaSeaview],
+  't100-qatar': [qatarLusailSunset],
 }
 
 export function pickShowcaseImage(raceId?: string) {
