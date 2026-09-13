c9eb50dc5c15dd12716d65c7da7764e1f352e35e
shyandrey <shy.andrey@gmail.com>
2026-09-13 19:17:02 +0300

    Add athlete gender and country code fields

--- src/types/Athlete.ts
+++ src/types/Athlete.ts
@@ -1,10 +1,14 @@
+export type AthleteGender = 'M' | 'W'
+
 export type Athlete = {
   id: number
   name: string
   nameEn?: string
   country: string
   countryEn?: string
+  countryCode?: string
   flag: string
+  gender?: AthleteGender
   discipline: string
   image?: string
   bio: string
