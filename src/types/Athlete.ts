ea70bf8bc1170b897a5dd65213814a3cbbefb8cf
shyandrey <shy.andrey@gmail.com>
2026-09-14 01:33:19 +0300

    Add athlete age and social links

--- src/types/Athlete.ts
+++ src/types/Athlete.ts
@@ -1,5 +1,10 @@
 export type AthleteGender = 'M' | 'W'
 
+export type AthleteSocialLinks = {
+  instagram?: string
+  youtube?: string
+}
+
 export type Athlete = {
   id: number
   name: string
@@ -11,6 +16,8 @@ export type Athlete = {
   gender?: AthleteGender
   discipline: string
   image?: string
+  age?: number
+  socialLinks?: AthleteSocialLinks
   bio: string
   bioFacts?: string[]
   achievements: string[]
