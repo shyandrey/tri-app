c880ddab099d2dff0a445df8bf3d90a02dcab700
shyandrey <shy.andrey@gmail.com>
2026-09-16 00:34:00 +0300

    Add five more profiles to photo cleanup

--- scripts/import-athlete-photos.mjs
+++ scripts/import-athlete-photos.mjs
@@ -16,6 +16,7 @@ const VERIFIED_NO_PHOTO_NAMES = new Set([
   'Tommy Doubleday','Yvan Jarrige','Albert Askengren',
   'Ari Klau','Benjamin Randall','Brad Bischoff','Brian Folts','David Reynolds',
   'Dries Matthys','Dylan Clough','Dylan Thissen','Erwan Jacobi','Florin Parfuss',
+  'Fraser Minnican','Jack Sosinski','Jens Emil Nielsen','Joona Lehtonen','Stephanie Clutterbuck',
 ])
 const PROFILE_SLUG_OVERRIDES = {
   'Magnus Ditlev': 'magnus-elbaek-ditlev', 'Daniel Bækkegård': 'daniel-baekkegard', 'Kristian Høgenhaug': 'kristian-hogenhaug',
