62556f4bed87bd477718d5045724865d7f9ade45
shyandrey <shy.andrey@gmail.com>
2026-09-18 00:06:09 +0300

    Include result-derived athletes in catalog

--- src/data/athletes/index.ts
+++ src/data/athletes/index.ts
@@ -1,10 +1,12 @@
 import type { Athlete } from '../../types/Athlete'
 import { maleAthletes } from './men'
 import { femaleAthletes } from './women'
+import { resultAthletes } from './resultAthletes.generated'
 
 export const athletes: Athlete[] = [
   ...maleAthletes,
   ...femaleAthletes,
+  ...resultAthletes,
 ]
 
-export { maleAthletes, femaleAthletes }
+export { maleAthletes, femaleAthletes, resultAthletes }
