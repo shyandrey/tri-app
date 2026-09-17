047bb94956880926a3197074fcb97b06481c2ada
shyandrey <shy.andrey@gmail.com>
2026-09-17 21:50:19 +0300

    Mark six more women as no-photo

--- src/data/athletes/createAthlete.ts
+++ src/data/athletes/createAthlete.ts
@@ -10,6 +10,7 @@ const VERIFIED_NO_PHOTO_NAMES = new Set([
   'Fraser Minnican','Jack Sosinski','Jens Emil Nielsen','Joona Lehtonen','Stephanie Clutterbuck',
   'Abbie Sullivan','Adele Likin','Charlotte McShane','Nikita Paskiewiez','Sarah Karpinski',
   'Annette Rogers','Carolyn Olsen','Leslie Homol','Luisa Iogna Prat','Marissa Lovell',
+  'Rebecca Kawaoka','Shiva Leisner','Amber Ferreira','Anne Basso','Antonia Milowsky','Jana Uderstadt',
 ])
 
 export const makeAthlete = (
