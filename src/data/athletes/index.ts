e62a6b90d7dea0516beac2f8b08a6a868b5d926e
shyandrey <shy.andrey@gmail.com>
2026-09-18 00:20:17 +0300

    Include verified result athletes in catalog

--- src/data/athletes/index.ts
+++ src/data/athletes/index.ts
@@ -2,11 +2,13 @@ import type { Athlete } from '../../types/Athlete'
 import { maleAthletes } from './men'
 import { femaleAthletes } from './women'
 import { resultAthletes } from './resultAthletes.generated'
+import { verifiedResultAthletes } from './verifiedResultAthletes'
 
 export const athletes: Athlete[] = [
   ...maleAthletes,
   ...femaleAthletes,
   ...resultAthletes,
+  ...verifiedResultAthletes,
 ]
 
-export { maleAthletes, femaleAthletes, resultAthletes }
+export { maleAthletes, femaleAthletes, resultAthletes, verifiedResultAthletes }
