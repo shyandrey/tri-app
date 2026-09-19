2d949d8d3180b7ade9d0791fa4bf7516a85fdade
shyandrey <shy.andrey@gmail.com>
2026-09-19 20:44:10 +0300

    Apply registered photos to generated athletes

--- src/data/athletes/index.ts
+++ src/data/athletes/index.ts
@@ -3,12 +3,18 @@ import { maleAthletes } from './men'
 import { femaleAthletes } from './women'
 import { resultAthletes } from './resultAthletes.generated'
 import { verifiedResultAthletes } from './verifiedResultAthletes'
+import { athletePhotosByName } from './athletePhotos.generated'
+
+const withRegisteredPhoto = (athlete: Athlete): Athlete => ({
+  ...athlete,
+  image: athlete.image ?? athletePhotosByName[athlete.nameEn],
+})
 
 export const athletes: Athlete[] = [
   ...maleAthletes,
   ...femaleAthletes,
   ...resultAthletes,
   ...verifiedResultAthletes,
-]
+].map(withRegisteredPhoto)
 
 export { maleAthletes, femaleAthletes, resultAthletes, verifiedResultAthletes }
